3e6f6bd2096e72def0022639d33c46645b519ed6
Michael Rommel <michael.rommel@siemens-healthineers.com>
2023-05-05 21:50:49 +0200

    feat: add unique prefix to title names

--- lua/tabline.lua
+++ lua/tabline.lua
@@ -3,87 +3,131 @@
 
 local M = {}
 local fn = vim.fn
+local map = vim.tbl_map
+local split = vim.split
 
 M.options = {
-    show_index = true,
-    show_modify = true,
-    show_icon = false,
-    brackets = { '[', ']' },
-    no_name = 'No Name',
-    modify_indicator = ' [+]',
-    inactive_tab_max_length = 0
+	show_index = true,
+	show_modify = true,
+	show_icon = false,
+	brackets = { '[', ']' },
+	no_name = 'No Name',
+	modify_indicator = ' [+]',
+	inactive_tab_max_length = 0
 }
 
+M.buffer_prefixes = function()
+	-- adapted from https://github.com/willothy/nvim-cokeline/blob/master/lua/cokeline/buffers.lua
+	local buffers = fn.getbufinfo({ buflisted = 1 })
+
+	local paths = map(function(buffer)
+		return fn.reverse(split(buffer.name, "/"))
+	end, buffers)
+
+	local prefixes = map(function()
+		return {}
+	end, buffers)
+
+	for i = 1, #paths do
+		for j = i + 1, #paths do
+			local k = 1
+			while paths[i][k] == paths[j][k] and paths[i][k] do
+				k = k + 1
+				prefixes[i][k - 1] = prefixes[i][k - 1] or paths[i][k]
+				prefixes[j][k - 1] = prefixes[j][k - 1] or paths[j][k]
+			end
+			if k ~= 1 then
+				prefixes[i][k - 1] = prefixes[i][k - 1] or paths[i][k]
+				prefixes[j][k - 1] = prefixes[j][k - 1] or paths[j][k]
+			end
+		end
+	end
+
+	local bufprefix = {}
+
+	for i, buffer in ipairs(buffers) do
+		buffer.unique_prefix = table.concat({
+			#prefixes[i] == #paths[i] and "/" or "",
+			fn.join(fn.reverse(prefixes[i]), "/"),
+			#prefixes[i] > 0 and "/" or "",
+		})
+		bufprefix[buffer.bufnr] = buffer.unique_prefix
+	end
+
+	return bufprefix
+end
+
 local function tabline(options)
-    local s = ''
-    for index = 1, fn.tabpagenr('$') do
-        local winnr = fn.tabpagewinnr(index)
-        local buflist = fn.tabpagebuflist(index)
-        local bufnr = buflist[winnr]
-        local bufname = fn.bufname(bufnr)
-        local bufmodified = fn.getbufvar(bufnr, '&mod')
+	local s = ''
+	local bp = M.buffer_prefixes()
+	for index = 1, fn.tabpagenr('$') do
+		local winnr = fn.tabpagewinnr(index)
+		local buflist = fn.tabpagebuflist(index)
+		local bufnr = buflist[winnr]
+		local bufname = fn.bufname(bufnr)
+		local bufmodified = fn.getbufvar(bufnr, '&mod')
 
-        s = s .. '%' .. index .. 'T'
-        if index == fn.tabpagenr() then
-            s = s .. '%#TabLineSel#'
-        else
-            s = s .. '%#TabLine#'
-        end
-        -- tab index
-        s = s .. ' '
-        -- index
-        if options.show_index then
-            s = s .. index .. ':'
-        end
-        -- icon
-        local icon = ''
-        if options.show_icon and M.has_devicons then
-            local ext = fn.fnamemodify(bufname, ':e')
-            icon = M.devicons.get_icon(bufname, ext, { default = true }) .. ' '
-        end
-        -- buf name
-        s = s .. options.brackets[1]
-        local pre_title_s_len = string.len(s)
-        if bufname ~= '' then
-            s = s .. icon .. fn.fnamemodify(bufname, ':t')
-        else
-            s = s .. options.no_name
-        end
-        if options.inactive_tab_max_length
-            and options.inactive_tab_max_length > 0
-            and index ~= fn.tabpagenr()
-        then
-            s = string.sub(s, 1, pre_title_s_len + options.inactive_tab_max_length)
-        end
-        s = s .. options.brackets[2]
-        -- modify indicator
-        if
-            bufmodified == 1
-            and options.show_modify
-            and options.modify_indicator ~= nil
-        then
-            s = s .. options.modify_indicator
-        end
-        -- additional space at the end of each tab segment
-        s = s .. ' '
-    end
+		s = s .. '%' .. index .. 'T'
+		if index == fn.tabpagenr() then
+			s = s .. '%#TabLineSel#'
+		else
+			s = s .. '%#TabLine#'
+		end
+		-- tab index
+		s = s .. ' '
+		-- index
+		if options.show_index then
+			s = s .. index .. ':'
+		end
+		-- icon
+		local icon = ''
+		if options.show_icon and M.has_devicons then
+			local ext = fn.fnamemodify(bufname, ':e')
+			icon = M.devicons.get_icon(bufname, ext, { default = true }) .. ' '
+		end
+		-- buf name
+		s = s .. options.brackets[1]
+		local pre_title_s_len = string.len(s)
+		if bufname ~= '' then
+			s = s .. icon .. (bp[bufnr] ~= nil and bp[bufnr] or "") .. fn.fnamemodify(bufname, ':t')
+		else
+			s = s .. options.no_name
+		end
+		if options.inactive_tab_max_length
+			and options.inactive_tab_max_length > 0
+			and index ~= fn.tabpagenr()
+		then
+			s = string.sub(s, 1, pre_title_s_len + options.inactive_tab_max_length)
+		end
+		s = s .. options.brackets[2]
+		-- modify indicator
+		if
+			bufmodified == 1
+			and options.show_modify
+			and options.modify_indicator ~= nil
+		then
+			s = s .. options.modify_indicator
+		end
+		-- additional space at the end of each tab segment
+		s = s .. ' '
+	end
 
-    s = s .. '%#TabLineFill#'
-    return s
+	s = s .. '%#TabLineFill#'
+	return s
 end
 
 function M.setup(user_options)
-    M.options = vim.tbl_extend('force', M.options, user_options)
-    M.has_devicons, M.devicons = pcall(require, 'nvim-web-devicons')
+	M.options = vim.tbl_extend('force', M.options, user_options)
+	M.has_devicons, M.devicons = pcall(require, 'nvim-web-devicons')
 
-    function _G.nvim_tabline()
-        return tabline(M.options)
-    end
+	function _G.nvim_tabline()
+		return tabline(M.options)
+	end
 
-    vim.o.showtabline = 2
-    vim.o.tabline = '%!v:lua.nvim_tabline()'
+	vim.o.showtabline = 2
+	vim.o.tabline = '%!v:lua.nvim_tabline()'
 
-    vim.g.loaded_nvim_tabline = 1
+	vim.g.loaded_nvim_tabline = 1
 end
 
 return M
